4141044aa4e14d0e842dadc018498936951bf864
Dinithi Imalsha <145284809+D-2020483@users.noreply.github.com>
2024-05-15 11:20:06 +0530

    Add files via upload

---
+++ full-stack/ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All rights reserved 2024 by Dinithi</span>
+
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
---
+++ full-stack/ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-primary'>
+            <a className="navbar-brand" href="https://www.ems.net">Employee Management System</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
+
